65d839fc725ae7fa25cc98873aa03e4dcfb716fa
maxthemus <max.somerille62@gmail.com>
2023-10-01 14:55:01 +1300

    changes made to backend to fix production

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables from .env file
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000; 
+  console.log(port);
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
